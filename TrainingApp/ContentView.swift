545fa4cf69809e770e771a952055d7a07438a374
Bhavananda <buliekov.vlad@gmail.com>
2024-06-03 10:14:14 +0300

    Initial Commit

---
+++ TrainingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrainingApp
+//
+//  Created by Bhavananda Das on 03.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrainingApp/TrainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingAppApp.swift
+//  TrainingApp
+//
+//  Created by Bhavananda Das on 03.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
