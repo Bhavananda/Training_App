49e11e630132cb755fe4f53480c482818cf87396
Bhavananda <buliekov.vlad@gmail.com>
2024-06-06 14:39:23 +0300

    100 % done

---
+++ TrainingApp/View/Elements/SettingCell.swift
@@ -0,0 +1,18 @@
+//
+//  SettingCell.swift
+//  TrainingApp
+//
+//  Created by Bhavananda Das on 06.06.2024.
+//
+
+import SwiftUI
+
+struct SettingCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingCell()
+}
